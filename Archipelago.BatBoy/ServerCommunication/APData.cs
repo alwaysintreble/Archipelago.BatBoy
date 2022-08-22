75ba0cbf10465f09bf156abcf3f15184124db44f
alwaysintreble <mmmcheese158@gmail.com>
2022-08-22 13:32:27 -0500

    setup ap data

---
+++ Archipelago.BatBoy/ServerCommunication/APData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Archipelago.BatBoy;
+
+public class APData
+{
+    public long Index;
+    public string HostName;
+    public string SlotName;
+    public string Password;
+    public HashSet<long> @Checked = new HashSet<long>();
+}
